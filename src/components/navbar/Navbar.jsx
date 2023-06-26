85eb62a7ac2749ab9c35e2004204429a9463e611
Shawn Oloye <shawnoloye@gmail.com>
2023-06-26 15:07:52 +0100

    fourth commit

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -5,8 +5,6 @@ import Link from 'next/link';
 import { MenuIcon, XIcon } from '@heroicons/react/outline';
 import NavDropdown from '../navDropdown/navDropdown';
 
-
-
 const Navbar = () => {
   const [nav, setNav] = useState(false);
   const handleClick = () => setNav(!nav);
@@ -14,13 +12,18 @@ const Navbar = () => {
   const handleClose = () => setNav(!nav);
 
   return (
-    <div className="w-full h-[60px] sticky top-0 z-10 bg-white focus:outline-none focus:ring-4 focus:ring-purple-300 fixed drop-shadow-lg br-transparent ">
-      <div className="px-2 flex justify-between items-center w-full h-full">
-        <h1 className="text-3xl font-bold mr-4 sm:text-4xl">BRAND.</h1>
-        <div className="flex justify-between items-center ">
-          <ul className="hidden md:flex space-x-6 ">
+    <div className="br-transparent fixed sticky top-0 z-10 h-[60px] w-full bg-white drop-shadow-lg focus:outline-none focus:ring-4 focus:ring-purple-300 ">
+      <div className="flex h-full w-full items-center justify-between px-2">
+        <h1 className="mr-4 text-3xl font-bold sm:text-4xl">BRAND.</h1>
+        <div className="flex items-center justify-between ">
+          <ul className="hidden space-x-6 md:flex ">
             <li>
-              <Link className='transform hover:scale-105 transition-all duration-300' href="/" smooth={+true} duration={500}>
+              <Link
+                className="transform transition-all duration-300 hover:scale-105"
+                href="/"
+                smooth={+true}
+                duration={500}
+              >
                 Home
               </Link>
             </li>
@@ -35,7 +38,7 @@ const Navbar = () => {
               </Link>
             </li>
             <li>
-              <Link href="/forgotPassword" smooth={+true} offset={-100} duration={500}>
+              <Link href="/" smooth={+true} offset={-100} duration={500}>
                 Platforms
               </Link>
             </li>
@@ -45,14 +48,13 @@ const Navbar = () => {
               </Link>
             </li>
           </ul>
-          <NavDropdown/>
-          
+          <NavDropdown />
         </div>
-        <div className="hidden md:flex pr-4">
+        <div className="hidden pr-4 md:flex">
           <Link href="/login" smooth={+true} offset={-50} duration={500}>
             <button
               type="button"
-              className=" text-white bg-purple-700 hover:bg-purple-800 ease-in focus:outline-none  focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-3 mr-2 mb-2 dark:bg-purple-500 dark:hover:bg-purple-500 dark:focus:ring-purple-500 dark:border-gray-700"
+              className=" mb-2 mr-2 rounded-full bg-purple-700 px-5  py-3 text-sm font-medium text-white ease-in hover:bg-purple-800 focus:outline-none focus:ring-gray-300 dark:border-gray-700 dark:bg-purple-500 dark:hover:bg-purple-500 dark:focus:ring-purple-500"
             >
               LogIn
             </button>
@@ -64,24 +66,24 @@ const Navbar = () => {
           <Link href="/signup" smooth={+true} offset={-50} duration={500}>
             <button
               type="button"
-              className="text-white bg-purple-700 hover:bg-purple-800 focus:outline-none ease-in focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-3 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+              className="mb-2 mr-2 rounded-full bg-purple-700 px-5 py-3 text-sm font-medium text-white ease-in hover:bg-purple-800 focus:outline-none focus:ring-gray-300 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700"
             >
               SignUp
             </button>
           </Link>
         </div>
-        <div className="md:hidden mr-4" onClick={handleClick}>
+        <div className="mr-4 md:hidden" onClick={handleClick}>
           {!nav ? <MenuIcon className="w-5" /> : <XIcon className="w-5" />}
         </div>
       </div>
 
-      <ul className={!nav ? 'hidden' : 'absolute bg-zinc-200 w-full px-8'}>
-        <li className="border-b-2 border-zinc-300 w-full">
+      <ul className={!nav ? 'hidden' : 'absolute w-full bg-zinc-200 px-8'}>
+        <li className="w-full border-b-2 border-zinc-300">
           <Link onClick={handleClose} href="/" smooth={+true} duration={500}>
             Home
           </Link>
         </li>
-        <li className="border-b-2 border-zinc-300 w-full">
+        <li className="w-full border-b-2 border-zinc-300">
           <Link
             onClick={handleClose}
             href="/"
@@ -92,7 +94,7 @@ const Navbar = () => {
             About
           </Link>
         </li>
-        <li className="border-b-2 border-zinc-300 w-full">
+        <li className="w-full border-b-2 border-zinc-300">
           <Link
             onClick={handleClose}
             href="/"
@@ -103,7 +105,7 @@ const Navbar = () => {
             Support
           </Link>
         </li>
-        <li className="border-b-2 border-zinc-300 w-full">
+        <li className="w-full border-b-2 border-zinc-300">
           <Link
             onClick={handleClose}
             href="/"
@@ -114,7 +116,7 @@ const Navbar = () => {
             Platforms
           </Link>
         </li>
-        <li className="border-b-2 border-zinc-300 w-full">
+        <li className="w-full border-b-2 border-zinc-300">
           <Link
             onClick={handleClose}
             href="/"
@@ -125,20 +127,18 @@ const Navbar = () => {
             Pricing
           </Link>
         </li>
-        <li className="border-b-2 border-zinc-300 w-full">
+        <li className="w-full border-b-2 border-zinc-300">
           <Link
             onClick={handleClose}
             href="/"
             smooth={+true}
             offset={-50}
             duration={500}
-          >
-           
-          </Link>
+          ></Link>
         </li>
 
-        <div className="flex flex-col my-4">
-          <button className="bg-transparent text-indigo-600 px-8 py-3 mb-4">
+        <div className="my-4 flex flex-col">
+          <button className="mb-4 bg-transparent px-8 py-3 text-indigo-600">
             Sign In
           </button>
           <button className="px-8 py-3">Sign Up</button>
--- src/components/stuff/Stuff.jsx
+++ src/components/stuff/Stuff.jsx
@@ -4,8 +4,6 @@ import Image from 'next/image';
 
 const Stuff = () => {
   return (
-    
-
     <section className="body-font text-gray-600">
       <div className="container mx-auto flex flex-wrap px-5 py-24">
         <div className="mb-20 flex w-full flex-wrap">
@@ -13,7 +11,8 @@ const Stuff = () => {
             Our Gallery
           </h1>
           <p className="mx-auto text-base leading-relaxed lg:w-2/3 lg:pl-6">
-            Our gallery showcases the vibrant and inspiring moments of the student educational journeys. Step into the world of our borrowers
+            Our gallery showcases the vibrant and inspiring moments of the
+            student educational journeys. Step into the world of our borrowers
             and witness their transformative experiences as they pursue their
             dreams in universities across the globe. Through captivating
             photographs and heartfelt testimonials, our gallery captures the
